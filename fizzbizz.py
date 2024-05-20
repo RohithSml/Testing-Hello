d3d8317b60ba04c03495304cb08475745c7e451c
RohithSml <rohith.sml94@gmail.com>
2024-05-20 12:53:02 +0530

    Ads fizbizz

---
+++ fizzbizz.py
@@ -0,0 +1,13 @@
+def fizzbizz(n):
+    print("Heloo", n)
+if __name__ == "__main__":
+    # n = int(input("Enter a number: "))
+    fizzbizz(5)
+
+
+# Write a python function called fizzbizz which will take a number n as input 
+# and print numbers i from 1 to n.
+# If i is divisible by 3, it will print fizz instead of i
+# If i is divisible by 5, it will print bizz instead of i
+# If i is divisible by 15, it will print fizzbizz instead i
+# Otherwise, it will just print i
